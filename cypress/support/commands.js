7e1e1f40777e13d72638ffd97f165faf9273a248
jens <jensramakers@gmail.com>
2024-03-27 20:25:34 +0100

    added tests to implement ci/cd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,19 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login', (role) => {
+    cy.visit('http://localhost:5173/login')
+
+    cy.intercept("/config.json", { fixture: 'config.json' }).as('getConfig');
+    cy.intercept("https://localhost:44305/api/v1/Category", { fixture: 'categories.json' }).as('getCategories');
+
+    cy.get('#email').type(`${role}@gmail.com`);
+    cy.get('#password').type('password123456789');
+    cy.get('#login-button').click();
+
+    cy.intercept('POST','https://localhost:44305/api/Login', { statusCode: 200, fixture: 'loginAdmin.json' }).as('loginAdmin');
+    cy.wait('@loginAdmin');
+
+    cy.intercept('GET', 'https://localhost:44305/api/v1/Category', { fixture: 'categories.json' }).as('getCategories');
+});
--- src/Auth/Register/RegisterPage.jsx
+++ src/Auth/Register/RegisterPage.jsx
@@ -85,9 +85,9 @@ function RegisterPage() {
                                         onChange={(e) => handleFormChange(e.target.value, "email")}
                                         type="text"
                                         className="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                                        id="exampleFormControlInput2"
+                                        id="email"
                                         placeholder="Email address"/>
-                                    <label htmlFor="exampleFormControlInput2"
+                                    <label htmlFor="email"
                                            className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary">
                                         Email address
                                     </label>
@@ -98,10 +98,10 @@ function RegisterPage() {
                                         onChange={(e) => handleFormChange(e.target.value, "password")}
                                         type="password"
                                         className="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                                        id="exampleFormControlInput22"
+                                        id="password"
                                         placeholder="Password"/>
                                     <label
-                                        htmlFor="exampleFormControlInput22"
+                                        htmlFor="password"
                                         className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary">
                                         Password
                                     </label>
@@ -112,10 +112,10 @@ function RegisterPage() {
                                         onChange={(e) => handleFormChange(e.target.value, "confirmPassword")}
                                         type="password"
                                         className="peer block min-h-[auto] w-full rounded border bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                                        id="exampleFormControlInput222"
+                                        id="password-confirm"
                                         placeholder="Password"/>
                                     <label
-                                        htmlFor="exampleFormControlInput222"
+                                        htmlFor="password-confirm"
                                         className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary">
                                         Confirm Password
                                     </label>
@@ -130,6 +130,7 @@ function RegisterPage() {
                                     }
 
                                     <button
+                                        id={"register-button"}
                                         className="inline-block w-full rounded bg-primary px-7 pb-2 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
                                         data-twe-ripple-init=""
                                         data-twe-ripple-color="light">
