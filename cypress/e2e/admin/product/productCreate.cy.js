59cd391ed1c4da9c6c9c27b87e81345b03daf2f8
jens <jensramakers@gmail.com>
2024-04-06 17:54:19 +0200

    added bad flows

--- cypress/e2e/admin/product/productCreate.cy.js
+++ cypress/e2e/admin/product/productCreate.cy.js
@@ -21,4 +21,25 @@ describe('Load Admin Product Create', () => {
 
         cy.get('[data-cy="product-row"]').should('be.visible');
     })
+
+    it('fails validation', () => {
+        cy.intercept("GET","https://localhost:44305/api/v1/Category", { statusCode: 200, fixture: 'categories.json' }).as('getCategories');
+        cy.intercept("POST","https://localhost:44305/api/v1/Product", { statusCode: 201, fixture: 'productCreate.json' }).as('createProduct');
+        cy.intercept("GET","https://localhost:44305/api/v1/Product", { statusCode: 200, fixture: 'products.json' }).as('getProducts');
+
+        cy.login('admin');
+
+        cy.visit('http://localhost:5173/admin/products/create');
+
+        cy.get('[data-cy="product-create"]').should('be.visible');
+
+        cy.get('#name').type('Test Product');
+        cy.get('#description').type('Test Description');
+        cy.get('#image').selectFile('cypress/used_uploads/lilo.png');
+        cy.get('#category').select('2');
+
+        cy.get('[data-cy="product-submit"]').click();
+
+        cy.get('[data-cy="form-errors"]').should('be.visible');
+    })
 })
